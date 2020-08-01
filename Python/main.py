aacefe35de709b4bf70a3f6b4de5052f36ec2008
JLob Lap <joselobatof@gmail.com>
2020-07-31 21:39:48 -0500

    Constructor con Python

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,11 @@
 from car import Car
+from account import Account
 
 def run():
-    car = Car()
-    car.license = "ASD1232"
-    car.driver = "Enrique Perez"
+    car = Car("ASD1232", Account("Enrique Perez", "EP123"))
+    car.passengers = 4
     print(vars(car))
+    print(vars(car.driver))
 
 if __name__ == "__main__":
     run()
