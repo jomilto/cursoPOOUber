5019eef4313d07d4c1437dea056d0e42b25865b7
JLob Lap <joselobatof@gmail.com>
2020-08-02 15:09:54 -0500

    Herencia con JS

--- JS/Index.js
+++ JS/Index.js
@@ -1,3 +1,7 @@
 var car = new Car("WAE123", new Account("Enrique Perez", "dasd123123"))
 car.passengers = 4;
 car.printDataCar();
+
+var uberX = new UberX("WAE123", new Account("Enrique Perez", "dasd123123"),"Chevrolet","Spark")
+uberX.passengers = 4;
+uberX.printDataCar();
